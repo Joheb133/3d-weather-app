d5e8ce8570861be3f5bf9bea12a6cc08e9b33c47
zoheb598 <joheb133@gmail.com>
2023-02-20 14:36:44 +0000

    add function that moves weather model
    -Moves the right model (determined by weather res icon) to above the right country

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import scene from './3d/scene';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Object3D, ObjectLoader } from 'three';
+import * as THREE from 'three';
 import rotateAroundSphere from './3d/utils/earthRotation';
 import search from './api/search';
 import {createUl} from './utils/searchList';
+import { Object3D } from 'three';
 
 
 //load scene
@@ -20,11 +21,12 @@ gltfLoader.load( 'models/earth-low-poly_lq/earth.gltf', function ( gltf ) {
 
 //add weather models to scene
 interface Obj3ds {
-    group?: THREE.Group,
-    obj: {[key: string]: Object3D}
+    group: THREE.Group,
+    obj: {[key: string]: THREE.Object3D}
 }
 
 let obj3ds: Obj3ds = {
+    group: new THREE.Group(),
     obj: {}
 }
 
@@ -40,45 +42,57 @@ const dictionary: { [key: string]: any } = {
     'mist': '50'
 }
 
-const objectLoader = new ObjectLoader()
-objectLoader.load("data/model.json", function (obj) {
+const objectLoader = new THREE.ObjectLoader()
+objectLoader.load('data/model.json', function (obj) {
     const children = obj.children
     
     children.forEach(element => {
-        element.scale.set(0.2, 0.2, 0.2)
+        element.scale.set(0.15, 0.15, 0.15)
         element.position.set(0, 0, 0)
         //obj key = (dictionary value = element name) : value = element
         obj3ds.obj[dictionary[element.name]] = element
     })
-
-    obj3ds.group = obj as THREE.Group
-    myScene.add(obj3ds.group)
+    obj3ds.group.children = children
+    obj3ds.group.name = 'weather';
+    myScene.add(obj3ds.group);
 })
 
+function moveIcon(no: string, lat: number, lon: number) {
+    //reset asset positions
+    for(const key in obj3ds.obj) {
+        obj3ds.obj[key].position.set(0, 0, 0);
+    }
+
+    //format res icon string
+    no = no.replace(no.charAt(2), '')
+
+    rotateAroundSphere(obj3ds.obj[no], lat, lon, 0.5, false)
+    
+}
 
 
 // handle searching weather
-const locInput = document.getElementById("location-input") as HTMLInputElement;
-const submitBtn = document.getElementById("submit-button") as HTMLButtonElement;
+const locInput = document.getElementById('location-input') as HTMLInputElement;
+const submitBtn = document.getElementById('submit-button') as HTMLButtonElement;
 const tempEl = document.querySelector('#temp-el') as HTMLSpanElement;
 let searching = false;
 
-submitBtn.addEventListener("click", async function() {
+submitBtn.addEventListener('click', async function() {
     if(searching) return
     searching = true
 
     let input = locInput.value;
-    let country = "";
+    let country = '';
     //if ul res already exists
     if(document.querySelector('.response')) {
         document.querySelector('.response')?.remove();
     }
 
     //if user specify country
-    if(locInput.value.includes(",")) {
-        country = input.slice(input.indexOf(",")+1, input.length)
+    if(locInput.value.includes(',')) {
+        country = input.slice(input.indexOf(',')+1, input.length)
         country = country.trimStart()
-        input = input.slice(0, input.indexOf(","))
+        input = input.slice(0, input.indexOf(','))
     }
 
     const city = await search(input);
@@ -96,9 +110,9 @@ submitBtn.addEventListener("click", async function() {
             locInput.value = '';
             temp.value = value.main.temp
             addTempEl();
-            rotateAroundSphere(obj3ds.group!, value.coord.lat, value.coord.lon, 0)
+            rotateAroundSphere(obj3ds.group, value.coord.lat, value.coord.lon, 12, false)
+            moveIcon(value.weather[0].icon, value.coord.lat, value.coord.lon)
             rotateAroundSphere(myScene.camera, value.coord.lat, value.coord.lon, 10)
-            console.log(obj3ds.group?.position, myScene.camera.position);
             
         }).catch((error)=>{
             console.log(error)
@@ -109,15 +123,15 @@ submitBtn.addEventListener("click", async function() {
 });
 
 //temperature conversion
-const tempBtn = document.querySelector("#temp-toggle-btn") as HTMLButtonElement;
+const tempBtn = document.querySelector('#temp-toggle-btn') as HTMLButtonElement;
 let temp = {
     c: true,
     f: false,
-    value: "" as any
+    value: '' as any
 }
 
 tempBtn.addEventListener("click", ()=>{
-    if(temp.value == "") return 
+    if(temp.value == '') return 
     if(temp.c) {
         temp.c = false;
         temp.f = true;
