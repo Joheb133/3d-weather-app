77b5e403e67be59c10eac3041f65d0fed4d2ea4c
zoheb598 <joheb133@gmail.com>
2023-05-07 03:10:32 +0100

    setup weather vercel serverless
    -installed vercel -g

---
+++ api/weather.js
@@ -0,0 +1,18 @@
+import needle from 'needle';
+import url from 'url';
+
+export default async function weather(req, res) {
+    try {
+        const params = new URLSearchParams({
+            [process.env.API_KEY_NAME]: process.env.API_KEY_VALUE,
+            ...url.parse(req.url, true).query,
+        })
+        const apiRes = await needle('get', `https://api.openweathermap.org/data/2.5/weather?${params}`);
+        const data = apiRes.body
+
+        res.setHeader('Cache-Control', 'max-age=0, s-maxage=300')
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
