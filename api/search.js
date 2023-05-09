bc9993cb8362f9975313c32175ad13f5eecdbdee
zoheb598 <joheb133@gmail.com>
2023-05-10 00:16:45 +0100

    add serverless search api caching

--- api/search.js
+++ api/search.js
@@ -1,7 +1,8 @@
 //serverless city search
 
-import needle from 'needle';
+import needle from 'needle'
 import zlib from 'zlib'
+import cache from 'memory-cache'
 
 function constant(data, search) {
     let searchList = data[search]
@@ -11,21 +12,28 @@ function constant(data, search) {
     return searchList
 }
 
-let CACHE = await needle('get', `https://raw.githubusercontent.com/Joheb133/3d-weather-app/main/data/hash_city.json.gz`)
+const CACHE_DURATION_MS = 60 * 60 * 1000; // 1 hour
+const CACHE_KEY = 'city_data';
 
 export default async function search(req, res) {
     try {
-        if (CACHE.statusCode !== 200) {
-            CACHE = await needle('get', `https://raw.githubusercontent.com/Joheb133/3d-weather-app/main/data/hash_city.json.gz`)
-            res.status(500).json('Failed to load JSON file')
-        } else {
-            const decompressed = zlib.gunzipSync(CACHE.body)
-            const data = JSON.parse(decompressed)
-            //return value in lowercase nondiacritic (no laten symbols like fada)
-            const search = req.query.name.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
+        let data = cache.get(CACHE_KEY);
 
-            res.status(200).json(constant(data, search))
+        if (!data) {
+            const response = await needle('get', `https://raw.githubusercontent.com/Joheb133/3d-weather-app/main/data/hash_city.json.gz`);
+            if (response.statusCode !== 200) {
+                res.status(500).json('Failed to load JSON file')
+                return;
+            } else {
+                const decompressed = zlib.gunzipSync(response.body);
+                data = JSON.parse(decompressed);
+                cache.put(CACHE_KEY, data, CACHE_DURATION_MS);
+            }
         }
+
+        //return value in lowercase nondiacritic (no laten symbols like fada)
+        const search = req.query.name.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "");
+        res.status(200).json(constant(data, search));
     } catch (error) {
         res.status(500).json(error.message)
     }
